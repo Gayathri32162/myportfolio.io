2e31d65bc274250646fd86274a56d41f52a7a92a
Gayathri32162 <gayathribalamurugan82@gmail.com>
2021-08-07 09:48:14 +0530

    this is my portfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent = document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
+
+// Hamburger Menu events
+hamburgerMenu.addEventListener('click', ()=>{
+    navContent.classList.add('show');
+    document.body.style.overflow="hidden";
+  });
+  closeNavButton.addEventListener('click', ()=>{
+    navContent.classList.remove('show');
+    document.body.style.overflow="initial";
+  });
